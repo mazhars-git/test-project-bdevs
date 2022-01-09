70243dbb6815c8595d04f3edbe619768d6c7deb6
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-01-10 01:43:20 +0600

    update Menu

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -2,13 +2,14 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import '../Css/MainMenu.css';
+import Logo from '../../images/logo.png';
 
 const MainMenu = () => {
     return (
         <section>
         <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid">
-                    <a class="navbar-brand brand-heading" href="/home">WeDo</a>
+                    <a class="navbar-brand brand-heading" href="/home"><img className='img-fluid' src={Logo} alt="Logo" /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -18,29 +19,31 @@ const MainMenu = () => {
                             <a class="nav-link" href="/home">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#service">Service</a>
+                            <a class="nav-link" href="#service">Event</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#works">Works</a>
+                            <a class="nav-link" href="#works">Prayers</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#about">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Clients</a>
+                            <a class="nav-link" href="#">Causes</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Blog</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#contact">Contact</a>
+                            <a class="nav-link" href="#contact">Sermons</a>
                         </li>
                     </ul>
+
+                    <button className='btn btn-primary'>Donate Now</button>
                     
-                    <div class="search-box">
+                    {/* <div class="search-box">
                         <span class="icon"><FontAwesomeIcon icon={faSearch} /></span>
                         <input type="search" id="search" placeholder="Search..." />
-                    </div>
+                    </div> */}
                     
                 </div>
             </div>
