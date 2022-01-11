49cfb6c6d29774360e945d3aa220f034c1fb908d
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-01-11 21:35:49 +0600

    about done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,13 +7,16 @@ const About = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-4">
-                        <h4>About Us</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <div className="about-content">
+                            <h4 className="about-heading">About Us</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        </div>
+                        
                     </div>
                     <div className="col-md-4">
                         <div className="row">
                             <div className="col-md-6">
-                                <h4>Company</h4>
+                                <h4 className="about-heading">Company</h4>
                                 <ul>
                                     <li><a href="#">About Us</a></li>
                                     <li><a href="#">Team</a></li>
@@ -23,7 +26,7 @@ const About = () => {
                                 </ul>
                             </div>
                             <div className="col-md-6">
-                                <h4>Services</h4>
+                                <h4 className="about-heading">Services</h4>
                                 <ul>
                                     <li><a href="#">Donation</a></li>
                                     <li><a href="#">Medical Service</a></li>
@@ -36,13 +39,15 @@ const About = () => {
                         
                     </div>
                     <div className="col-md-4">
-                        <h4>Recent Sermons</h4>
-                        <span>19 HOURS AGO</span>
-                        <p>Lorem ipsum is a dummy Content is read
-                        </p>
-                        <span>19 HOURS AGO</span>
-                        <p>Lorem ipsum is a dummy Content is read
-                        </p>
+                        <div className="recent-sm">
+                            <h4 className="about-heading pb-3">Recent Sermons</h4>
+
+                            <span>19 HOURS AGO</span>
+                            <p>Lorem ipsum is a dummy <br/> Content is read</p>
+
+                            <span>19 HOURS AGO</span>
+                            <p>Lorem ipsum is a dummy <br/> Content is read</p>
+                        </div>
                     </div>
                 </div>
             </div>
