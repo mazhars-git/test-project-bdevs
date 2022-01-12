73d6d52a24c477963c3a09fe4eaa110265a0435c
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-01-12 17:27:48 +0600

    contact form done

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,33 +1,34 @@
 import React from 'react';
+import '../Css/Contact.css';
 
 const Contact = () => {
     return (
-        <section>
+        <section className="contact">
             <div className="container py-5">
-                <p>Join With Us</p>
-                <h1>Get Involve With Us <br/>
+                <p className="semi-heading pt-5">Join With Us</p>
+                <h1 className="heading pb-3 text-center text-white">Get Involve With Us <br/>
                     & Made A Beautiful Globe
                 </h1>
-                <form className="row">
+                <form className="row contact-form">
                     <div class="col-md-6">
-                        <label for="firstName" class="form-label">YOUR FIRST NAME</label>
-                        <input type="text" class="form-control" id="firstName" />
+                        <label for="firstName" class="form-label">YOUR FIRST NAME <span>**</span></label>
+                        <input type="text" class="form-control" id="firstName" placeholder='Enter Your First Name....'/>
                     </div>
                     <div class="col-md-6">
-                        <label for="lastName" class="form-label">YOUR LAST NAME</label>
-                        <input type="text" class="form-control" id="lastName" />
+                        <label for="lastName" class="form-label">YOUR LAST NAME <span>**</span></label>
+                        <input type="text" class="form-control" id="lastName" placeholder='Enter Your Last Name....' />
                     </div>
                     <div class="col-md-6">
-                        <label for="email" class="form-label">EMAIL</label>
-                        <input type="email" class="form-control" id="email" />
+                        <label for="email" class="form-label">EMAIL <span>**</span></label>
+                        <input type="email" class="form-control" id="email" placeholder='Enter Your Email...' />
                     </div>
                     <div class="col-md-6">
-                        <label for="phone" class="form-label">YOUR PHONE NUMBER</label>
-                        <input type="tel" class="form-control" id="phone" />
+                        <label for="phone" class="form-label">YOUR PHONE NUMBER <span>**</span></label>
+                        <input type="tel" class="form-control" id="phone" placeholder='+(880)' />
                     </div>
                     <div class="col-md-6">
-                        <label for="religion" class="form-label">RELIGION</label>
-                        <select id="religion" class="form-select">
+                        <label for="religion" class="form-label">RELIGION <span>**</span></label>
+                        <select id="religion" class="form-select select">
                         <option selected>Select Religion</option>
                         <option>Islam</option>
                         <option>Hindu</option>
@@ -35,17 +36,17 @@ const Contact = () => {
                         </select>
                     </div>
                     <div class="col-md-4">
-                        <label for="inputState" class="form-label">SELECT LOCATION</label>
-                        <select id="inputState" class="form-select">
+                        <label for="inputState" class="form-label">SELECT LOCATION <span>**</span></label>
+                        <select id="inputState" class="form-select select">
                         <option selected>Country</option>
                         <option>USA</option>
                         <option>UK</option>
                         <option>LONDON</option>
                         </select>
                     </div>
-                    <div class="col-md-2 mt-2">
+                    <div class="col-md-2 mt-3">
                         <label for="inputState" class="form-label"></label>
-                        <select id="inputState" class="form-select">
+                        <select id="inputState" class="form-select select">
                         <option selected>State</option>
                         <option>New York</option>
                         <option>UK</option>
@@ -53,8 +54,8 @@ const Contact = () => {
                         </select>
                     </div>
                    
-                    <div class="col-12 text-center pt-5">
-                        <button type="submit" class="btn btn-primary">Join Right Now</button>
+                    <div class="col-12 text-center py-5">
+                        <button type="submit" class="btn btn-brand">Join Right Now</button>
                     </div>
                     
                 </form>
